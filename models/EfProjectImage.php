d252770e7e9fdb0ca075cdc58d8871d08b9d77fe
anawatom <anawat.om@gmail.com>
2015-09-06 13:15:29 +0700

    Update the model.

--- models/EfProjectImage.php
+++ models/EfProjectImage.php
@@ -12,7 +12,9 @@ use yii\behaviors\TimestampBehavior;
  *
  * @property integer $PROJECT_IMAGE_ID
  * @property integer $PROJECT_ID
+ * @property string $ABSOLUTE_IMAGE_PATH
  * @property string $IMAGE_PATH
+ * @property string $FILE_NAME
  * @property string $THUMBNAIL_IMAGE_PATH
  * @property string $IMAGE_DESC
  * @property integer $CREATE_BY
@@ -52,10 +54,10 @@ class EfProjectImage extends ActiveRecord
     public function rules()
     {
         return [
-            [['PROJECT_IMAGE_ID', 'PROJECT_ID', 'IMAGE_PATH', 'THUMBNAIL_IMAGE_PATH', 'CREATE_BY', 'CREATE_DATE', 'LAST_UPD_BY', 'LAST_UPD_DATE'], 'required'],
+            [['PROJECT_IMAGE_ID', 'PROJECT_ID', 'IMAGE_PATH', 'THUMBNAIL_IMAGE_PATH', 'CREATE_BY', 'LAST_UPD_BY'], 'required'],
             [['PROJECT_IMAGE_ID', 'PROJECT_ID', 'CREATE_BY', 'LAST_UPD_BY'], 'integer'],
             [['CREATE_DATE', 'LAST_UPD_DATE'], 'safe'],
-            [['IMAGE_PATH', 'THUMBNAIL_IMAGE_PATH', 'IMAGE_DESC'], 'string', 'max' => 255]
+            [['ABSOLUTE_IMAGE_PATH', 'IMAGE_PATH', 'FILE_NAME', 'THUMBNAIL_IMAGE_PATH', 'IMAGE_DESC'], 'string', 'max' => 255]
         ];
     }
 
@@ -67,7 +69,9 @@ class EfProjectImage extends ActiveRecord
         return [
             'PROJECT_IMAGE_ID' => Yii::t('app', 'Project  Image  ID'),
             'PROJECT_ID' => Yii::t('app', 'Project  ID'),
+            'ABSOLUTE_IMAGE_PATH' => Yii::t('app', 'Absolute Image Path'),
             'IMAGE_PATH' => Yii::t('app', 'Image  Path'),
+            'FILE_NAME' => Yii::t('app', 'File Name'),
             'THUMBNAIL_IMAGE_PATH' => Yii::t('app', 'Thumbnail  Image  Path'),
             'IMAGE_DESC' => Yii::t('app', 'Image  Desc'),
             'CREATE_BY' => Yii::t('app', 'Create  By'),
