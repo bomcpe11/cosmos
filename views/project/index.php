78eec5a422144c2afdbe38acfb323b695292a32b
anawatom <anawat.om@gmail.com>
2015-09-06 16:14:57 +0700

    Move the create button to GridView.

--- views/project/index.php
+++ views/project/index.php
@@ -15,54 +15,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Ef Project', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php 
-    //  echo GridView::widget([
-    //     'dataProvider' => $dataProvider,
-    //     'filterModel' => $searchModel,
-    //     'columns' => [
-    //         ['class' => 'yii\grid\SerialColumn'],
-
-    //         'PROJECT_ID',
-    //         'FISCAL_YEAR',
-    //         'PROJECT_TYPE_ID',
-    //         'PLAN_NAME',
-    //         'MAIN_PRODUCTIVITY',
-    //         // 'PROJECT_NAME',
-    //         // 'UNIT_ID',
-    //         // 'DIVISION_ID',
-    //         // 'START_DATE',
-    //         // 'END_DATE',
-    //         // 'BUDGET_TYPE_ID',
-    //         // 'BUDGET_RECEIVE',
-    //         // 'BUDGET_ACTUAL',
-    //         // 'PROJECT_STATUS',
-    //         // 'PROJ_HDLR_ID',
-    //         // 'CONTRACT_NUM',
-    //         // 'PLACE',
-    //         // 'AMPHOE_CODE',
-    //         // 'PROVINCE_CODE',
-    //         // 'PRINC_N_REASON:ntext',
-    //         // 'OBJECTIVE:ntext',
-    //         // 'TARGET:ntext',
-    //         // 'TARGET_GROUP:ntext',
-    //         // 'OUTPUT:ntext',
-    //         // 'INDICATOR:ntext',
-    //         // 'RESULT:ntext',
-    //         // 'SCOPE:ntext',
-    //         // 'PLAN:ntext',
-    //         // 'CREATE_BY',
-    //         // 'CREATE_DATE',
-    //         // 'LAST_UPD_BY',
-    //         // 'LAST_UPD_DATE',
-
-    //         ['class' => 'yii\grid\ActionColumn'],
-    //     ],
-    // ]); ?>
-
     <?= GridView::widget([
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
