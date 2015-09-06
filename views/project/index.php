e7f32fcee33af12e075c67822d204173617cdcbd
anawatom <anawat.om@gmail.com>
2015-09-06 16:13:50 +0700

    Add a GridView to the project index page.

--- views/project/index.php
+++ views/project/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\EfProjectSearch */
@@ -19,47 +19,139 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Ef Project', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
+    <?php 
+    //  echo GridView::widget([
+    //     'dataProvider' => $dataProvider,
+    //     'filterModel' => $searchModel,
+    //     'columns' => [
+    //         ['class' => 'yii\grid\SerialColumn'],
+
+    //         'PROJECT_ID',
+    //         'FISCAL_YEAR',
+    //         'PROJECT_TYPE_ID',
+    //         'PLAN_NAME',
+    //         'MAIN_PRODUCTIVITY',
+    //         // 'PROJECT_NAME',
+    //         // 'UNIT_ID',
+    //         // 'DIVISION_ID',
+    //         // 'START_DATE',
+    //         // 'END_DATE',
+    //         // 'BUDGET_TYPE_ID',
+    //         // 'BUDGET_RECEIVE',
+    //         // 'BUDGET_ACTUAL',
+    //         // 'PROJECT_STATUS',
+    //         // 'PROJ_HDLR_ID',
+    //         // 'CONTRACT_NUM',
+    //         // 'PLACE',
+    //         // 'AMPHOE_CODE',
+    //         // 'PROVINCE_CODE',
+    //         // 'PRINC_N_REASON:ntext',
+    //         // 'OBJECTIVE:ntext',
+    //         // 'TARGET:ntext',
+    //         // 'TARGET_GROUP:ntext',
+    //         // 'OUTPUT:ntext',
+    //         // 'INDICATOR:ntext',
+    //         // 'RESULT:ntext',
+    //         // 'SCOPE:ntext',
+    //         // 'PLAN:ntext',
+    //         // 'CREATE_BY',
+    //         // 'CREATE_DATE',
+    //         // 'LAST_UPD_BY',
+    //         // 'LAST_UPD_DATE',
+
+    //         ['class' => 'yii\grid\ActionColumn'],
+    //     ],
+    // ]); ?>
+
     <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                            [
+                                'class'=>'kartik\grid\SerialColumn',
+                                'contentOptions'=>['class'=>'kartik-sheet-style'],
+                                'width'=>'36px',
+                                'header'=>'',
+                                'headerOptions'=>['class'=>'kartik-sheet-style']
+                            ],
 
-            'PROJECT_ID',
-            'FISCAL_YEAR',
-            'PROJECT_TYPE_ID',
-            'PLAN_NAME',
-            'MAIN_PRODUCTIVITY',
-            // 'PROJECT_NAME',
-            // 'UNIT_ID',
-            // 'DIVISION_ID',
-            // 'START_DATE',
-            // 'END_DATE',
-            // 'BUDGET_TYPE_ID',
-            // 'BUDGET_RECEIVE',
-            // 'BUDGET_ACTUAL',
-            // 'PROJECT_STATUS',
-            // 'PROJ_HDLR_ID',
-            // 'CONTRACT_NUM',
-            // 'PLACE',
-            // 'AMPHOE_CODE',
-            // 'PROVINCE_CODE',
-            // 'PRINC_N_REASON:ntext',
-            // 'OBJECTIVE:ntext',
-            // 'TARGET:ntext',
-            // 'TARGET_GROUP:ntext',
-            // 'OUTPUT:ntext',
-            // 'INDICATOR:ntext',
-            // 'RESULT:ntext',
-            // 'SCOPE:ntext',
-            // 'PLAN:ntext',
-            // 'CREATE_BY',
-            // 'CREATE_DATE',
-            // 'LAST_UPD_BY',
-            // 'LAST_UPD_DATE',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
+                            [
+                                'attribute' => 'FISCAL_YEAR', 
+                                'vAlign' => 'middle', 
+                                'filterType' => GridView::FILTER_SELECT2,
+                                'filter' => ['2558'=>'2558', '2557'=>'2557', '2556'=>'2556'], 
+                                'filterWidgetOptions'=>[
+                                    'pluginOptions'=>['allowClear'=>true, 'width'=>'auto'],
+                                ],
+                                'filterInputOptions'=>['placeholder'=>''],
+                            ] ,
+                            'PLAN_NAME',
+                            //'จัดกลุ่ม',
+                            [
+                                'attribute'=>'PROJECT_STATUS', 
+                                'value'=> function ($model, $key, $index, $widget) {
+                                                return Yii::$app->params['projectStatuses'][$model->PROJECT_STATUS];
+                                            },
+                                'vAlign'=>'middle', 
+                                'filterType'=>GridView::FILTER_SELECT2,
+                                'filter'=> Yii::$app->params['projectStatuses'], 
+                                'filterWidgetOptions'=>[
+                                    'pluginOptions'=>['allowClear'=>true, 'width'=>'auto'],
+                                ],
+                                'filterInputOptions'=>['placeholder'=>''],
+                            ],
+                            'BUDGET_ACTUAL',
+                     
+                            [
+                                'class' => 'kartik\grid\ActionColumn',
+                                'template' => '{view} {update} {delete}',
+                            ],
+            ],
+            'containerOptions'=> [
+                    'style'=>'overflow: auto'
+            ], 
+            'headerRowOptions'=> [
+                    'class'=>'kartik-sheet-style'
+            ],
+            'filterRowOptions'=> [
+                    'class'=>'kartik-sheet-style'
+            ],
+            'pjax'=>false,
+            'toolbar'=> [
+                    [
+                        'content'=> Html::a ('<i class="glyphicon glyphicon-plus"></i>',
+                                                ['create'], 
+                                                [   'class' => 'btn btn-success activity-view-link',
+                                                    'title' => Yii::t('yii', 'View'),
+                                                    'data-value' => 'index.php?r=menu-main/create',                    
+                                                ]
+                                            )
+                                    . ' ' .                       
+                                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', 
+                                            ['index'], 
+                                            [
+                                                'data-pjax'=>0, 
+                                                'class'=>'btn btn-default', 
+                                                'title'=>'Reset Grid'
+                                            ])          
+                    ] ,                                          
+                    '{export}',
+                    '{toggleData}',
+            ] , 
+            'export'=>['fontAwesome'=>true] ,
+            'bordered'=>true,
+            'striped'=>true,
+            'condensed'=>true,
+            'responsive'=>true,
+            'hover'=>true,
+            'showPageSummary'=>true,
+            'panel'=> [
+                'heading' => 'เมนูหลัก', 
+                'type'=>GridView::TYPE_PRIMARY,
+            ],
+            'persistResize'=>false,
+            'export' => false,                                                       
     ]); ?>
+</div>
 
 </div>
