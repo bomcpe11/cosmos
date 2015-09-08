1272c8709f62e7a33f037fe5825ee9753d92bb0e
anawatom <anawat.om@gamil.com>
2015-09-08 21:47:49 +0700

    Remove the kartik-v font extension.
    :)

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -354,13 +354,4 @@ return array (
       '@kartik/date' => $vendorDir . '/kartik-v/yii2-widget-datepicker',
     ),
   ),
-  'kartik-v/yii2-icons' => 
-  array (
-    'name' => 'kartik-v/yii2-icons',
-    'version' => '9999999-dev',
-    'alias' => 
-    array (
-      '@kartik/icons' => $vendorDir . '/kartik-v/yii2-icons',
-    ),
-  ),
 );
